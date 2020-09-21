ac394762fa87a975ef8cda7fc2e905c43e65573e
Gregor Martynus <gregor@martynus.net>
2020-09-21 14:23:21 -0700

    feat: initial version

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { VERSION } from "./version";
+
+type Octokit = any;
+type Options = {
+  [option: string]: any;
+};
+
+/**
+ * @param octokit Octokit instance
+ * @param options Options passed to Octokit constructor
+ */
+export function config(octokit: Octokit, options: Options) {}
+config.VERSION = VERSION;
