probot/octokit-plugin-config

commit a9c9ed4d3496b77f49411b6e5006c4cc7090be34

feat!: package is now ESM (#249)

scripts/build.mjs
@@ -12,6 +12,8 @@ const sharedOptions = {
   minify: false,
   allowOverwrite: true,
   packages: "external",
+  platform: "neutral",
+  format: "esm",
 };
 
 async function main() {
@@ -22,10 +24,7 @@ async function main() {
     entryPoints: await globby(["./src/*.ts", "./src/**/*.ts"]),
     outdir: "pkg/dist-src",
     bundle: false,
-    platform: "neutral",
-    format: "esm",
     ...sharedOptions,
-    sourcemap: false,
   });
 
   // Remove the types file from the dist-src folder
@@ -38,28 +37,6 @@ async function main() {
   }
 
   const entryPoints = ["./pkg/dist-src/index.js"];
-
-  await Promise.all([
-    // Build the a CJS Node.js bundle
-    esbuild.build({
-      entryPoints,
-      outdir: "pkg/dist-node",
-      bundle: true,
-      platform: "node",
-      target: "node14",
-      format: "cjs",
-      ...sharedOptions,
-    }),
-    // Build an ESM browser bundle
-    esbuild.build({
-      entryPoints,
-      outdir: "pkg/dist-web",
-      bundle: true,
-      platform: "browser",
-      format: "esm",
-      ...sharedOptions,
-    }),
-  ]);
 
   // Copy the README, LICENSE to the pkg folder
   await copyFile("LICENSE", "pkg/LICENSE");
@@ -78,10 +55,16 @@ async function main() {
       {
         ...pkg,
         files: ["dist-*/**", "bin/**"],
-        main: "dist-node/index.js",
-        module: "dist-web/index.js",
+        // Tooling currently are having issues with the "exports" field, ex: TypeScript, eslint
+        // We add a `main` and `types` field to the package.json for the time being
+        main: "dist-src/index.js",
         types: "dist-types/index.d.ts",
-        source: "dist-src/index.js",
+        exports: {
+          ".": {
+            types: "./dist-src/index.js",
+            import: "./dist-src/index.js",
+          },
+        },
         sideEffects: false,
       },
       null,

src/compose-config-get.ts
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/core";
 
-import { getConfigFiles } from "./util/get-config-files";
-import type { Configuration, GetOptions, GetResult } from "./types";
+import { getConfigFiles } from "./util/get-config-files.js";
+import type { Configuration, GetOptions, GetResult } from "./types.js";
 
 /**
  * Loads configuration from one or multiple files and resolves with

src/index.ts
@@ -1,8 +1,8 @@
 import { Octokit } from "@octokit/core";
 
-import { VERSION } from "./version";
-import { composeConfigGet } from "./compose-config-get";
-import type * as Types from "./types";
+import { VERSION } from "./version.js";
+import { composeConfigGet } from "./compose-config-get.js";
+import type * as Types from "./types.js";
 
 /**
  * @param octokit Octokit instance
@@ -19,7 +19,7 @@ export function config(octokit: Octokit): Types.API {
 
 config.VERSION = VERSION;
 
-export { composeConfigGet } from "./compose-config-get";
+export { composeConfigGet } from "./compose-config-get.js";
 
 export namespace createPullRequest {
   export type GetOptions<T extends Types.Configuration = Types.Configuration> =

src/util/get-config-file.ts
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/core";
 import yaml from "js-yaml";
 
-import type { Configuration, ConfigFile } from "../types";
+import type { Configuration, ConfigFile } from "../types.js";
 
 type Options = {
   owner: string;

src/util/get-config-files.ts
@@ -1,8 +1,8 @@
 import { Octokit } from "@octokit/core";
 
-import type { ConfigFile } from "../types";
-import { getConfigFile } from "./get-config-file";
-import { extendsToGetContentParams } from "./extends-to-get-content-params";
+import type { ConfigFile } from "../types.js";
+import { getConfigFile } from "./get-config-file.js";
+import { extendsToGetContentParams } from "./extends-to-get-content-params.js";
 
 type Options = {
   owner: string;

test/get.test.ts
@@ -2,8 +2,8 @@ import { Octokit } from "@octokit/core";
 import fetchMock from "fetch-mock";
 import stripIndent from "strip-indent";
 
-import { config } from "../src";
-import { Configuration } from "../src/types";
+import { config } from "../src/index.js";
+import type { Configuration } from "../src/types.js";
 
 const TestOctokit = Octokit.plugin(config);
 

test/issues.test.ts
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/core";
 import fetchMock from "fetch-mock";
 
-import { config } from "../src";
+import { config } from "../src/index.js";
 
 const TestOctokit = Octokit.plugin(config);
 

test/smoke.test.ts
@@ -1,4 +1,4 @@
-import { config, composeConfigGet } from "../src";
+import { config, composeConfigGet } from "../src/index.js";
 
 describe("Smoke test", () => {
   it("config is a function", () => {

commit 960898656a2be74a7921b131e73a99f9a3b33df8

fix: Export types (#251)
This actually exports the types, which was accidentally broken in #249.

scripts/build.mjs
@@ -61,7 +61,7 @@ async function main() {
         types: "dist-types/index.d.ts",
         exports: {
           ".": {
-            types: "./dist-src/index.js",
+            types: "./dist-types/index.d.ts",
             import: "./dist-src/index.js",
           },
         },